5cd5ac09f60bd945df1f08a0f0dd0636e7359c94
eugenemonnier <eugene.monnier@gmail.com>
2020-01-14 21:00:18 -0800

    darksky daily weather info populated

--- server.js
+++ server.js
@@ -3,41 +3,53 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // allows server to talk to frontend
 app.use(cors());
 
-let localWeather = [];
 // routes
+
+// don't forget to...
+app.get('/JulyTalk', (request, response) => {
+  response.redirect('https://www.youtube.com/watch?v=YWywb9i-z7Y');
+});
+
 app.get('/location', (request, response) => {
   try {
     let city = request.query.city;
     const geoData = require('./data/geo.json');
     let geoDataResults  = geoData[0];
     let locations = new Location(city, geoDataResults);
-    // search_query: city,
-    // formatted_query: geoData[0].display_name,
-    // latitude: geoData[0].lat,
-    // longitude: geoData[0].lon
     response.status(200).send(locations);
   }
 
   catch(error) {
-    errorHandler('we messed up', response);
+    errorHandler('we messed up', request, response);
   }
 });
 
-app.get('/weather', (response) => {
-  const weatherData = require('./data/darksky.json');
-  console.log(weatherData[6]);
-  let weatherDataResults  = weatherData[6][2];
-  new Weather(weatherDataResults);
-  console.log(localWeather);
+app.get('/weather', (request, response) => {
+  try {
+    const weatherData = require('./data/darksky.json');
+    const localWeather  = [];
+    weatherData.daily.data.forEach(dayData => {
+      localWeather.push(new Weather(dayData));
+    });
+    response.status(200).send(localWeather);
+  }
+  catch(error) {
+    errorHandler('we messed up', request, response);
+  }
 });
 
+// 404 route
+app.get('*', (request,response) => {
+  response.status(404).send(`This requested url ${request} does not exist on this server.`);
+});
+
+// constructors 
 function Location(city, locationData) {
   this.search_query = city;
   this.formatted_query = locationData.display_name;
@@ -45,27 +57,15 @@ function Location(city, locationData) {
   this.longitude = locationData.lon;
 }
 
-function Weather(locationData) {
-  locationData.forEach(locationData => {
-    let today = {};
-    today['forecast'] = locationData.summary;
-    today['time'] = new Date(locationData.time);
-    console.log(today);
-    localWeather.push(today);
-  });
+function Weather(dayData) {
+  this.forecast = dayData.summary;
+  this.time = new Date(dayData.time * 1000).toString().slice(0,15);
 }
 
+//error handler
 function errorHandler(string,response){
   response.status(500).send(string);
 }
-
-app.get('/JulyTalk', (request, response) => {
-  response.redirect('https://www.youtube.com/watch?v=YWywb9i-z7Y');
-});
-
-app.get('*', (request,response) => {
-  response.status(404).send('This route does not exist');
-})
 
 // turn it on
 app.listen(PORT, () => {
